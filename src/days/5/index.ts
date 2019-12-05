d885d560902813adadafff5773778ac7e68efb30
Henri Normak <henri.normak@gmail.com>
2019-12-05 07:50:53 +0200

    feat: day 5 and improved IntcodeComputer

---
+++ src/days/5/index.ts
@@ -0,0 +1,23 @@
+import { readInput } from '../../lib/input';
+import { IntcodeComputer } from '../../lib/intcode';
+
+async function partOne() {
+  const computer = new IntcodeComputer(1);
+  await computer.initialiseFromFile('./input.txt', __dirname);
+  computer.run();
+  return computer.getOutput();
+}
+
+async function partTwo() {
+  const computer = new IntcodeComputer(5);
+  await computer.initialiseFromFile('./input.txt', __dirname);
+  computer.run();
+  return computer.getOutput();
+}
+
+async function solve() {
+  // console.log('Result of part one', await partOne());
+  console.log('Result of part two', await partTwo());
+}
+
+solve().catch(console.error);
--- src/lib/intcode.ts
+++ src/lib/intcode.ts
@@ -3,20 +3,30 @@ import { readInput, InputOptions } from './input';
 enum OpCode {
   ADD = 1,
   MULTIPLY = 2,
-  SUBTRACT = -1,
-  DIVIDE = -2,
+  STORE = 3,
+  OUTPUT = 4,
+  JUMP_TRUE = 5,
+  JUMP_FALSE = 6,
+  LESS_THAN = 7,
+  EQUALS = 8,
   HALT = 99
+}
+
+enum ParameterMode {
+  POSITION = 0,
+  IMMEDIATE = 1,
 }
 
 export class IntcodeComputer {
   private memory: number[]
   private instructionPointer: number;
-  private noun: number;
-  private verb: number;
   private originalMemory: number[];
+  private output: number[];
+  private input: number;
 
-  constructor(input: { noun: number; verb: number } = { noun: 12, verb: 2 }) {
+  constructor(input: number = 0) {
     this.memory = [];
+    this.output = [];
     this.instructionPointer = 0;
     this.setInput(input);
   }
@@ -25,36 +35,91 @@ export class IntcodeComputer {
     this.memory = (await readInput(filePath, { relativeTo, splitLines: false }))[0].split(',').map((value) => parseInt(value, 10));
     this.originalMemory = [...this.memory];
     this.instructionPointer = 0;
-    this.memory[1] = this.noun;
-    this.memory[2] = this.verb;
   }
 
-  private handleOpCode(opCode: OpCode, memory: number[], position: number): { position: number, memory: number[] } {
+  async initialiseFromString(string: string) {
+    this.memory = string.split(',').map((value) => parseInt(value, 10));
+    this.originalMemory = [...this.memory];
+    this.instructionPointer = 0;
+  }
+
+  private handleOpCode(opCode: OpCode, parameterModes: ParameterMode[], memory: number[], position: number): { position: number, memory: number[] } {
     const result = [...memory];
+
+    console.log('Handle', opCode, parameterModes);
 
     switch (opCode) {
       case OpCode.ADD: {
         const length = 4;
         const [aAddr, bAddr, targetAddr] = memory.slice(position + 1, position + length);
-        result[targetAddr] = result[aAddr] + result[bAddr];
+        const a = parameterModes[0] === ParameterMode.IMMEDIATE ? aAddr : result[aAddr];
+        const b = parameterModes[1] === ParameterMode.IMMEDIATE ? bAddr : result[bAddr];
+
+        result[targetAddr] = a + b;
         return { position: position + length, memory: result };
       }
       case OpCode.MULTIPLY: {
         const length = 4;
         const [aAddr, bAddr, targetAddr] = memory.slice(position + 1, position + length);
-        result[targetAddr] = result[aAddr] * result[bAddr];
+        const a = parameterModes[0] === ParameterMode.IMMEDIATE ? aAddr : result[aAddr];
+        const b = parameterModes[1] === ParameterMode.IMMEDIATE ? bAddr : result[bAddr];
+
+        result[targetAddr] = a * b;
         return { position: position + length, memory: result };
       }
-      case OpCode.SUBTRACT: {
-        const length = 4;
-        const [aAddr, bAddr, targetAddr] = memory.slice(position + 1, position + length);
-        result[targetAddr] = result[aAddr] - result[bAddr];
+      case OpCode.STORE: {
+        const length = 2;
+        const [addr] = memory.slice(position + 1, position + length);
+        result[addr] = this.input;
         return { position: position + length, memory: result };
       }
-      case OpCode.DIVIDE: {
+      case OpCode.OUTPUT: {
+        const length = 2;
+        const [addr] = memory.slice(position + 1, position + length);
+        const a = parameterModes[0] === ParameterMode.IMMEDIATE ? addr : result[addr];
+        this.output.push(a);
+        return { position: position + length, memory: result };
+      }
+      case OpCode.JUMP_TRUE: {
+        const length = 3;
+        const [aAddr, bAddr] = memory.slice(position + 1, position + length);
+        const a = parameterModes[0] === ParameterMode.IMMEDIATE ? aAddr : result[aAddr];
+        const b = parameterModes[1] === ParameterMode.IMMEDIATE ? bAddr : result[bAddr];
+
+        if (a !== 0) {
+          return { position: b, memory: result };
+        }
+
+        return { position: position + length, memory: result };
+      }
+      case OpCode.JUMP_FALSE: {
+        const length = 3;
+        const [aAddr, bAddr] = memory.slice(position + 1, position + length);
+        const a = parameterModes[0] === ParameterMode.IMMEDIATE ? aAddr : result[aAddr];
+        const b = parameterModes[1] === ParameterMode.IMMEDIATE ? bAddr : result[bAddr];
+
+        if (a === 0) {
+          return { position: b, memory: result };
+        }
+
+        return { position: position + length, memory: result };
+      }
+      case OpCode.LESS_THAN: {
         const length = 4;
         const [aAddr, bAddr, targetAddr] = memory.slice(position + 1, position + length);
-        result[targetAddr] = result[aAddr] / result[bAddr];
+        const a = parameterModes[0] === ParameterMode.IMMEDIATE ? aAddr : result[aAddr];
+        const b = parameterModes[1] === ParameterMode.IMMEDIATE ? bAddr : result[bAddr];
+
+        result[targetAddr] = a < b ? 1 : 0;
+        return { position: position + length, memory: result };
+      }
+      case OpCode.EQUALS: {
+        const length = 4;
+        const [aAddr, bAddr, targetAddr] = memory.slice(position + 1, position + length);
+        const a = parameterModes[0] === ParameterMode.IMMEDIATE ? aAddr : result[aAddr];
+        const b = parameterModes[1] === ParameterMode.IMMEDIATE ? bAddr : result[bAddr];
+
+        result[targetAddr] = a === b ? 1 : 0;
         return { position: position + length, memory: result };
       }
       case OpCode.HALT:
@@ -62,24 +127,35 @@ export class IntcodeComputer {
     }
   }
 
-  private nextOpCode() {
+  private nextOpCode(): { opCode: OpCode, parameterModes: ParameterMode[] } {
     const opCode = this.memory[this.instructionPointer];
+    const stringOpCode = opCode.toString();
 
-    if (!isOpCode(opCode)) {
-      throw new Error('Unknown opcode');
+    if (stringOpCode.length <= 2) {
+      if (!isOpCode(opCode)) {
+        throw new Error('Unknown opcode');
+      }
+
+      return { opCode, parameterModes: [ParameterMode.POSITION, ParameterMode.POSITION, ParameterMode.POSITION] };
     }
 
-    return opCode;
+    // Parse out the parameter modes
+    const parameterModes = stringOpCode.slice(0, stringOpCode.length - 2).split('').map((val) => parseInt(val, 10)).reverse();
+    const pureOpCode = parseInt(stringOpCode.slice(stringOpCode.length - 2), 10);
+    return { opCode: pureOpCode, parameterModes: parameterModes };
   }
 
-  setInput({ verb, noun }: { verb: number; noun: number }) {
-    this.verb = verb;
-    this.noun = noun;
+  setInput(number: number) {
+    this.input = number;
+  }
+
+  getOutput() {
+    return [...this.output];
   }
 
   step() {
-    const opCode = this.nextOpCode();
-    const { position, memory } = this.handleOpCode(opCode, this.memory, this.instructionPointer);
+    const { opCode, parameterModes } = this.nextOpCode();
+    const { position, memory } = this.handleOpCode(opCode, parameterModes, this.memory, this.instructionPointer);
     this.instructionPointer = position;
     this.memory = memory;
 
@@ -87,11 +163,11 @@ export class IntcodeComputer {
   }
 
   run() {
-    let opCode = this.nextOpCode();
+    let { opCode } = this.nextOpCode();
 
     while (opCode !== OpCode.HALT) {
       this.step();
-      opCode = this.nextOpCode();
+      opCode = this.nextOpCode().opCode;
     }
 
     return this.memory;
@@ -99,8 +175,7 @@ export class IntcodeComputer {
 
   reset() {
     this.memory = [...this.originalMemory];
-    this.memory[1] = this.noun;
-    this.memory[2] = this.verb;
+    this.output = [];
     this.instructionPointer = 0;
   }
 }
