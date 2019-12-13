8aa5cc7a8b54a293e83b349e0be708a733ac5bde
Henri Normak <henri.normak@gmail.com>
2019-12-13 07:34:17 +0200

    feat: implement day 13

---
+++ src/days/13/index.ts
@@ -0,0 +1,85 @@
+import { IntcodeComputer } from '../../lib/intcode';
+
+enum Tile {
+  Empty = 0,
+  Wall = 1,
+  Block = 2,
+  HorizontalPaddle = 3,
+  Ball = 4,
+}
+
+async function initializeGame(playForFree?: boolean) {
+  const computer = new IntcodeComputer();
+  await computer.initialiseFromFile('./input.txt', __dirname);
+
+  if (playForFree) {
+    computer.overrideMemory(0, 2);
+  }
+
+  return computer;
+}
+
+function keyForCoordinate({ x, y }: { x: number; y: number }) {
+  return `${x}:${y}`;
+}
+
+async function partOne() {
+  const computer = await initializeGame();
+  const screen = new Map<string, Tile>();
+
+  while (!computer.isHalted()) {
+    computer.runUntilOutput();
+    computer.runUntilOutput();
+    const [x, y, tile] = computer.runUntilOutput().slice(-3);
+    screen.set(keyForCoordinate({ x, y }), tile);
+  }
+
+  return Array.from(screen.values()).filter((tile) => tile === Tile.Block).length;
+}
+
+async function partTwo(maxBlocks: number) {
+  const computer = await initializeGame(true);
+
+  let score = 0;
+  let ballPosition = { x: 0, y: 0 };
+  let paddlePosition = { x: 0, y: 0 };
+  const screen = new Map<string, Tile>();
+
+  while (!computer.isHalted()) {
+    computer.runUntilOutput();
+    computer.runUntilOutput();
+    const [x, y, tile] = computer.runUntilOutput().slice(-3);
+
+    // Quite a simple way to beat the game, just move paddle in response
+    // to the ball being moved
+    if (tile === Tile.HorizontalPaddle) {
+      paddlePosition = { x, y };
+    } else if (tile == Tile.Ball) {
+      ballPosition = { x, y };
+
+      if (ballPosition.x === paddlePosition.x) {
+        computer.setInput([0]);
+      } else if (ballPosition.x > paddlePosition.x) {
+        computer.setInput([1]);
+      } else if (ballPosition.x < paddlePosition.x) {
+        computer.setInput([-1]);
+      }
+    }
+
+    if (x == -1 && y === 0) {
+      score = tile;
+    } else {
+      screen.set(keyForCoordinate({ x, y }), tile);
+    }
+  }
+
+  return score;
+}
+
+async function solve() {
+  const blocks = await partOne();
+  console.log('Result of part one', blocks);
+  console.log('Result of part two', await partTwo(blocks));
+}
+
+solve().catch(console.error);
