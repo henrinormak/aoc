397230d1f4585fe4eccb0c7e15c206d3d70f02c3
Henri Normak <henri.normak@gmail.com>
2019-12-17 22:58:19 +0200

    refactor: add a function for finding sequence of commands

--- src/days/17/index.ts
+++ src/days/17/index.ts
@@ -1,7 +1,7 @@
 import { IntcodeComputer } from '../../lib/intcode';
 
-function findIntersections(image: string) {
-  const chars = image.split('\n').map((line) => line.split(''));
+function findIntersections(world: string) {
+  const chars = world.split('\n').map((line) => line.split(''));
   const intersections: { x: number, y: number }[] = [];
 
   const lines = chars.length;
@@ -22,9 +22,8 @@ function toInputs(string: string) {
   return string.split('').map((val) => val.charCodeAt(0));
 }
 
-async function partOne() {
-  const computer = new IntcodeComputer();
-  await computer.initialiseFromFile('./input.txt', __dirname);
+function getWorld(computer: IntcodeComputer) {
+  computer.overrideMemory(0, 1);
 
   let result = '';
 
@@ -35,21 +34,135 @@ async function partOne() {
     result += char;
   }
 
-  console.log('World');
-  console.log(result);
+  return result;
+}
 
-  const intersections = findIntersections(result);
+function findRobotIn(world: string) {
+  for (const [y, line] of world.split('\n').entries()) {
+    for (const [x, char] of line.split('').entries()) {
+      switch (char) {
+        case '<':
+          return { x, y, direction: 'L' };
+        case '>':
+          return { x, y, direction: 'R' };
+        case '^':
+          return { x, y, direction: 'U' };
+        case 'v':
+          return { x, y, direction: 'D' };
+      }
+    }
+  }
+
+  return undefined;
+}
+
+function canMoveForward({ x, y, direction }: { x: number; y: number, direction: string }, world: string[][]) {
+  switch (direction) {
+    case 'L':
+      return world[y][x - 1] === '#';
+    case 'R':
+      return world[y][x + 1] === '#';
+    case 'U':
+      return (world[y - 1] ?? [])[x] === '#';
+    case 'D':
+      return (world[y + 1] ?? [])[x] === '#';
+  }
+}
+
+function moveForward({ x, y, direction }: { x: number; y: number; direction: string }) {
+  switch (direction) {
+    case 'L':
+      return { x: x - 1, y, direction };
+    case 'R':
+      return { x: x + 1, y, direction };
+    case 'U':
+      return { x, y: y - 1, direction };
+    case 'D':
+      return { x, y: y + 1, direction };
+  }
+}
+
+function turn(direction: string, left: boolean) {
+  switch (direction) {
+    case 'L':
+      return left ? 'D' : 'U';
+    case 'R':
+      return left ? 'U' : 'D';
+    case 'U':
+      return left ? 'L' : 'R';
+    case 'D':
+      return left ? 'R' : 'L';
+  }
+}
+
+function getSequenceToSolve(world: string) {
+  const chars = world.split('\n').map((line) => line.split(''));
+  let robot = findRobotIn(world);
+
+  const sequence: string[] = [];
+  let forwardCounter = 0;
+
+  while (true) {
+    if (canMoveForward(robot, chars)) {
+      robot = moveForward(robot);
+      forwardCounter++;
+    } else if (canMoveForward({ ...robot, direction: turn(robot.direction, true) }, chars)) {
+      robot = { ...robot, direction: turn(robot.direction, true) };
+
+      if (forwardCounter > 0) {
+        sequence.push(forwardCounter.toString())
+      }
+      sequence.push('L');
+
+      forwardCounter = 0;
+    } else if (canMoveForward({ ...robot, direction: turn(robot.direction, false) }, chars)) {
+      robot = { ...robot, direction: turn(robot.direction, false) };
+
+      if (forwardCounter > 0) {
+        sequence.push(forwardCounter.toString())
+      }
+      sequence.push('R');
+
+      forwardCounter = 0;
+    } else {
+      // Nothing else to do, we are done (just flush out any last forward movements)
+      if (forwardCounter > 0) {
+        sequence.push(forwardCounter.toString())
+      }
+
+      return sequence.join(',');
+    }
+  }
+}
+
+async function partOne() {
+  const computer = new IntcodeComputer();
+  await computer.initialiseFromFile('./input.txt', __dirname);
+
+  const world = getWorld(computer);
+  console.log('World');
+  console.log(world);
+
+  const intersections = findIntersections(world);
   return intersections.reduce((sum, intersection) => sum + (intersection.x * intersection.y), 0);
 }
 
 async function partTwo() {
   const computer = new IntcodeComputer([]);
   await computer.initialiseFromFile('./input.txt', __dirname);
+
+  const world = getWorld(computer);
+  // Find the sequence to solve the path
+  const sequence = getSequenceToSolve(world);
+  console.log('Sequence', sequence);
+
+  computer.reset();
   computer.overrideMemory(0, 2);
 
-  // From part one, we know the sequence of operations we have to take is
-  // L,10,L,8,R,8,L,8,R,6,L,10,L,8,R,8,L,8,R,6,L,6,R,8,R,8,R,6,R,6,L,8,L,10,R,6,R,8,R,8,R,6,R,6,L,8,L,10,R,6,R,8,R,8,R,6,R,6,L,8,L,10,R,6,R,8,R,8,L,10,L,8,R,8,L,8,R,6
+  // From above, we know the sequence of operations we have to take is
   // This can be split into routines by hand (looking at the longest repeating patterns)
+  // L,10,L,8,R,8,L,8,R,6, L,10,L,8,R,8,L,8,R,6, R,6,R,8,R,8, R,6,R,6,L,8,L,10, R,6,R,8,R,8, R,6,R,6,L,8,L,10, R,6,R,8,R,8, R,6,R,6,L,8,L,10, R,6,R,8,R,8, L,10,L,8,R,8,L,8,R,6
+  // A,A,B,C,B,C,B,C,B,A
 
   computer.addInputs(toInputs('A,A,B,C,B,C,B,C,B,A\n'));  // Main
   computer.addInputs(toInputs('L,10,L,8,R,8,L,8,R,6\n')); // A
